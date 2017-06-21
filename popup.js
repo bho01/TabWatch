bf2948b3201719021e848c5a0124f38ad0a0ae6a
Ivan Chau <ivanmchau@gmail.com>
2017-06-21 19:43:27 -0400

    merge-conflict

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 const background = chrome.extension.getBackgroundPage();
 
 var global = {};
+var lastDate = Date.now();
 chrome.tabs.query({}, function(tabs){
 	background.console.log("yo")
 	background.console.log(tabs)
@@ -10,8 +11,14 @@ chrome.tabs.onCreated.addListener(function(tab){
 });
 chrome.tabs.onActivated.addListener(function(object){
 	console.log(object);
+	findOffset();
 });
 
+function findOffset(){
+	var offset = Date.now() - lastDate;
+	lastDate = Date.now();
+	background.console.log(offset);
+}
 class Session {
 	constructor(time, date){
 		this.time = time
