1eddd4824444e196756caa96b8fe0eee44ecfb6a
Ivan Chau <ivanmchau@gmail.com>
2017-06-21 20:07:39 -0400

    :D

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ const background = chrome.extension.getBackgroundPage();
 
 var global = {};
 var lastDate = Date.now();
+var lastTab = null;
 chrome.tabs.query({}, function(tabs){
 	background.console.log("yo")
 	background.console.log(tabs)
@@ -10,14 +11,34 @@ chrome.tabs.onCreated.addListener(function(tab){
 	console.log(tab);
 });
 chrome.tabs.onActivated.addListener(function(object){
-	console.log(object);
-	findOffset();
+	chrome.tabs.get(object.tabId, function(tab){
+		background.console.log(tab);
+		lastTab = tab;
+		var timeSpent = findOffset();
+		logTime(timeSpent, lastTab)
+	});
 });
 
+function logTime(timeSpent,tab){
+	var s = new Session(timeSpent[0],timeSpent[1]);
+	if(global[tab.title] == null){
+		var array = [];
+		array.push(s);
+		global[tab.title] = array
+	}else{
+		var array = global[tab.title];
+		array.push(s);
+		global[tab.title] = array;
+	}
+	console.log(global);
+}
+
 function findOffset(){
+	var beginning = lastDate;
 	var offset = Date.now() - lastDate;
 	lastDate = Date.now();
 	background.console.log(offset);
+	return [offset,beginning];
 }
 class Session {
 	constructor(time, date){
