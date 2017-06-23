82dbb430ab2b7792e52296d478ed420a36fdd72c
brendonho01 <brendon.ho01@gmail.com>
2017-06-22 23:23:00 -0400

    ayy

--- popup.js
+++ popup.js
@@ -59,19 +59,6 @@ function loadData(msg, number, callback){
     })
   });
 
-
-  var dialog = document.querySelector('dialog');
-  var showModalButton = document.querySelector('.show-modal');
-  if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-  }
-  showModalButton.addEventListener('click', function() {
-    dialog.showModal();
-  });
-  dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-  });
-});
 function manageClick(event, array){
   console.log(event)
   console.log(array)
